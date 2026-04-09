065cd5729de7274b2de9b476219c2bc943fcfd74
Cindy Orangis <cindyledev@gmail.com>
2026-04-09 11:10:14 -0400

    fix robots seo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -31,6 +31,10 @@ export const metadata: Metadata = {
   alternates: {
     canonical: 'https://www.snowplow.services',
   },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({
