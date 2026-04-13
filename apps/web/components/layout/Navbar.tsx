dfa71efeea81bf37f324f6791650c38c677fe83a
Cindy Orangis <cindyledev@gmail.com>
2026-04-13 13:57:59 -0400

    add /register href

--- apps/web/components/layout/Navbar.tsx
+++ apps/web/components/layout/Navbar.tsx
@@ -60,7 +60,7 @@ export default function Navbar() {
             Log in
           </Link>
           <Link
-            href="#"
+            href="/register"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
           >
             Sign up
@@ -85,7 +85,7 @@ export default function Navbar() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-gray-900 dark:sm:ring-gray-100/10">
           <div className="flex items-center gap-x-6">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Snow Plow Services</span>
               <Image
                 alt="tailwindcss logo"
@@ -103,7 +103,7 @@ export default function Navbar() {
               />
             </a>
             <a
-              href="#"
+              href="/register"
               className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
             >
               Sign up
